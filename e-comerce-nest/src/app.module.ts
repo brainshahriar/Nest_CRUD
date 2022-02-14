5dbb9dd1038edb2eb5772ab8b149f727f991d960
shahriar <shahriar.mehedi@bjitgroup.com>
2022-02-14 23:52:33 +0600

    cli

--- e-comerce-nest/src/app.module.ts
+++ e-comerce-nest/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot("mongodb+srv://shahriar:shahriarb33343536373839@cluster0.aushq.mongodb.net/ecommerce?retryWrites=true&w=majority")],
   controllers: [AppController],
   providers: [AppService],
 })
