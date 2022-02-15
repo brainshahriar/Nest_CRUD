50211f107c488cae21b7ff3ce33abc2ee82e6079
shahriar <shahriar.mehedi@bjitgroup.com>
2022-02-15 23:46:50 +0600

    aa

--- crud/src/app.module.ts
+++ crud/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [UserModule,
+  MongooseModule.forRoot('mongodb+srv://shahriar:shahriarb33343536373839@cluster0.aushq.mongodb.net/crud?retryWrites=true&w=majority')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
